76943ffc510bce4e7b1fe4d37ebdaed8bd13dbbe
claudius108 <claudius.teodorescu@gmail.com>
2014-12-20 11:07:03 +0200

    Added createGuid() function.
    References #706.

--- resources/scripts/query.js
+++ resources/scripts/query.js
@@ -2,6 +2,13 @@ tamboti = {};
 
 tamboti.selectedSearchResultOptions = {};
 
+tamboti.createGuid = function() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random()*16|0, v = c === 'x' ? r : (r&0x3|0x8);
+        return v.toString(16);
+    });
+} 
+
 $(function() {
     $('#keyword-form').submit(function () {
         loadIndexTerms();
