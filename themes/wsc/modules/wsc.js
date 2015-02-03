3ae359541bd0b70c7643908cbcc0d261bda81676
claudius108 <claudius.teodorescu@gmail.com>
2015-02-03 12:39:31 +0100

    Removed the theme 'wsc'.
    References #739.

--- themes/wsc/modules/wsc.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    $("#nav .hlist li").on("click", function(event){
-        event.stopPropagation();
-        var $this = $(this);
-        $this.siblings().removeClass('active');
-        $this.addClass('active');        
-        $('#content-placeholder').load($this.attr('id') + '.html #content-placeholder > *');  
-    });
-});
